4c7612f376abe2c06300a810fe191afca682934d
imamhsn195 <imamhsn195@gmail.com>
2021-12-04 14:52:06 +0400

    Part 1 finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      title:"This is a title",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("This is home page bar")
-        ),
-        body: const Center(
-          child: Text("Child of body"),
-        )
+    return const MaterialApp(
+      title:"Start Up Wallet Title",
+      home: RandomWords(),
+      );
+  }
+}
+
+class RandomWords extends StatefulWidget{
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+class _RandomWordsState extends State<RandomWords>{
+  @override
+  Widget build(BuildContext context){
+    // final wordPair = WordPair.random();
+    // return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text("Start Up Wallet App"),
+      ),
+      body: _buildSuggessions()
+    );
+  }
+    final _biggerFont = const TextStyle(fontSize: 18);
+    final _suggessions = <WordPair>[];
+  Widget _buildSuggessions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i){
+        if(i.isOdd){
+          return const Divider();
+        }
+          final index = i ~/ 2;
+          if(i >= _suggessions.length){
+            _suggessions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggessions[index]);
+      }
+    );
+  }
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
   }
